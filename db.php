e703bd6678ab4c787aba958f4ed7982fa6164688
Maddi08 <119130548+Maddi08@users.noreply.github.com>
2022-12-10 07:19:25 -0600

    Update db.php

--- db.php
+++ db.php
@@ -2,8 +2,9 @@
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
     //mysql://b61f85b58ead2a:76af0a4d@us-cdbr-east-06.cleardb.net/heroku_fb1f4cf96cceae5?reconnect=true
+   // mysql://b6fcee7192222d:01c9c419@us-cdbr-east-06.cleardb.net/heroku_bc3ccb8601b1e4b?reconnect=true
     //$con = mysqli_connect("127.0.0.1","root","","inventory");
-    $con=mysqli_connect("us-cdbr-east-06.cleardb.net","b61f85b58ead2a","76af0a4d","heroku_fb1f4cf96cceae5");
+    $con=mysqli_connect("us-cdbr-east-06.cleardb.net","b6fcee7192222d","01c9c419","heroku_bc3ccb8601b1e4b");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
